1a68c3f8e458e42756c2f8723bc8409a299fb793
Sreelakshmi Ajith <sreelakshmiajithn@gmail.com>
2024-07-19 13:40:52 +0530

    Add files via upload

--- sreeeelaksmi.c
+++ sreeeelaksmi.c
@@ -2,22 +2,45 @@
 void delay(unsigned int count);
 int main() {
 
-IODIR0 |= (1 << 2);
+IODIR0 |= (1 << 1);
+IODIR0 |= (1 << 3);
 IODIR0 |= (1 << 4);
-IODIR0 |= (1 << 7);
   // Set P0.10 as output
 
 while (1){
-        IOSET0 |= (1 << 2);
-					delay(500000);
-        IOCLR0 |= (1 << 2);	
-					IOSET0 |= (1 << 4);
-					delay(500000);
-	IOCLR0 |= (1 << 4);	
-	        IOSET0 |= (1 << 7);
-					delay(500000);
-	IOCLR0 |= (1 << 7);	
-    }
+	IOCLR0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOCLR0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+	IOCLR0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOCLR0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+delay(400000);
+	IOSET0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+  delay(400000);
+	IOSET0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+      }
 }
 void delay(unsigned int count)
 {
